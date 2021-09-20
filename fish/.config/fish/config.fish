23c359c121eaf0a63f14f0e5a416cd31be6ead8e
Lưu Minh Hoàng <hoang@Hoangs-MacBook-Pro.local>
2021-09-20 20:12:17 +0700

    Fish, Raycast update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+set -gx JAVA_HOME (/usr/libexec/java_home -v 11.0.9.1)
